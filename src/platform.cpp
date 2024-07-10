7eea1610762a8e31de8898cb19106752a1657062
Ubaid01 <Obaid.majeed2005@gmail.com>
2024-07-10 08:59:14 +0500

    Motioned Platforms

---
+++ src/platform.cpp
@@ -0,0 +1,91 @@
+#include "Platform.h"
+#include <cstdlib>
+#include <cmath>
+#include "player.h"
+
+Platform::Platform( int index , int level_mode ) 
+
+{
+    width = 100;
+    height = 32;
+    if ( level_mode == LM_EasyMode )
+        currentMotion = MT_Stationary ;
+	else 
+    {
+        currentMotion = static_cast<MotionTypes>( rand() % MT_MaxLimit ) ;
+		currentMotion = ( currentMotion > MT_Parabolic ) ? MT_Stationary : currentMotion ;
+    }
+    	
+    dx = dy = dt = 0.0 ;
+    vx = (rand() % 2 == 0) ? 1.25 : -1.25 ; 
+    vy = 0.75 ;
+
+    x = rand() % (SCREEN_WIDTH - width - 15) + 15 ;
+    y = 0 - height - (index * 100);
+
+    int coinInt = rand() % 4;
+
+    hasCoin = ( coinInt > 0 ) && ( index != 0 );
+
+    coinX = static_cast<int>(x + width / 2.0 - 12 );
+    coinY = static_cast<int>(y - 24 - 5);
+}
+
+void Platform::updatePosition( int level_mode ) 
+{
+    y += 1 ;
+    double previousX = x;
+	double previousY = y - 1 ;
+
+    dt += 0.05; 
+    if ( currentMotion == MT_Swaying ) 
+    {
+    	x += sin(dt) * vx ; 
+    	if (x < 0 || x + width > SCREEN_WIDTH)
+        {
+            vx = -vx;
+            x += vx * sin(dt);
+        }
+        dx = x - previousX;
+	}
+	else if ( currentMotion == MT_Parabolic )
+	{
+		x += sin( dt ) * vx ;
+		if (x < 0 || x + width > SCREEN_WIDTH)
+        {
+            vx = -vx;
+            x += vx * sin(dt);
+        }
+		y += cos( dt ) * vy ;
+		dx = x - previousX ;
+        dy = y - previousY;
+	}
+	else if (currentMotion == MT_Diagonal)
+    {
+        x += vx;
+        if ( x < 0 || x + width > SCREEN_WIDTH )
+        {
+            vx = -vx;
+            x += vx; // move back within bounds
+        }
+
+        dx = x - previousX;
+    }
+
+    coinX = static_cast<int>(x + width / 2.0 - 12);
+    coinY = static_cast<int>(y - 24 - 5);
+
+    if ( y > SCREEN_HEIGHT ) 
+	{
+        if ( level_mode == LM_AdventureMode )
+        {
+            currentMotion = static_cast<MotionTypes>(rand() % MT_MaxLimit);
+            currentMotion = (currentMotion > MT_Parabolic) ? MT_Stationary : currentMotion;
+        }
+        x = rand() % (SCREEN_WIDTH - width - 15) + 15 ;
+        y = 0 - height;
+
+        int coinInt = rand() % 4;
+        hasCoin = (coinInt > 0);
+    }
+}
---
+++ src/platform.h
@@ -0,0 +1,60 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define STARTING_GRAVITY 1.0
+
+enum MotionTypes
+{
+	MT_Stationary , MT_Swaying , MT_Diagonal , MT_Parabolic , MT_MaxLimit = 4
+};
+
+enum LevelMode
+{
+    LM_EasyMode = 1, LM_AdventureMode
+};
+	
+class Platform {
+public:
+    Platform() {}
+    Platform( int index , int level_mode = LM_EasyMode );
+
+    double getX() const { return x; }
+    double getChangedX( ) const { return dx ; }
+    double getChangedY() const { return dy; }
+    double getY() const { return y; }
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+
+    bool getHasCoin() const { return hasCoin; }
+    void setHasCoin(bool value) { hasCoin = value; }
+    int getCoinX() const { return coinX; }
+    int getCoinY() const { return coinY; }
+    
+	bool checkOverlap(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) 
+		{	return (x1 < x2 + w2) && (x1 + w1 > x2) && (y1 < y2 + h2) && (y1 + h1 > y2);  }
+    void block() { dx = 0 ; dy = 0 ; }
+    bool isPlatformHaveMotion( MotionTypes type ) const { return currentMotion == type ; }
+    
+    void updatePosition( int level_mode );
+    
+private:
+    double x;
+    double y;
+    double dx;
+    double dy;
+    double dt;
+    float vx ;
+    float vy ;
+    MotionTypes currentMotion ;
+    int width;
+    int height;
+    bool hasCoin;
+    int coinX;
+    int coinY;
+
+
+};
+
+#endif // PLATFORM_H
